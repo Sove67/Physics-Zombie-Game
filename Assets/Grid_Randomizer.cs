febc5ae96f6223829b0dc3e1bcfba09c96c3c3e9
Kaleb Afseth <afseth598@gmail.com>
2019-08-12 20:03:15 -0600

    Grid Randomizer
    Created the script for city generation to be based off of.

---
+++ Assets/Grid_Randomizer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Grid_Randomizer : MonoBehaviour
+{
+    public int gridHeight;
+    public int gridWidth;
+    public int randomizerRange;
+    public List<List<int>> row = new List<List<int>>();
+
+    public Text container;
+
+    public void Start()
+    {
+        Create();
+    }
+
+    public void Create()
+    {
+        for (int a = 0; a < gridWidth; a++)
+        {
+            List<int> collumn = new List<int>();
+
+            for (int b = 0; b < gridHeight; b++)
+            {
+                collumn.Add(Random.Range(0, (0 + randomizerRange)));
+            }
+
+            foreach (var item in collumn)
+            {
+                Debug.Log(item);
+            }
+
+            row.Add(collumn);
+
+            string text = "Grid:";
+            
+            foreach (var child in row)
+            {
+                text = text + "\n\n";
+                foreach (var item in child)
+                {
+                    text = text + item + " ";
+                }
+            }
+
+            container.text = text;
+        }
+    }
+}
