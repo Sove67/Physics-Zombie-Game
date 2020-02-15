f77ee6b288787fb81f815dffe8f7b08120012762
Kaleb Afseth <afseth598@gmail.com>
2020-02-14 18:06:32 -0600

    Backup
    I'm about to try and fix the problems i'm having by making more and want something to come back to

--- Assets/Scripts/Grid_Generator.cs
+++ Assets/Scripts/Grid_Generator.cs
@@ -5,65 +5,40 @@ using UnityEngine.UI;
 
 public class Grid_Generator : MonoBehaviour
 {
+    /*
+        Note To Self:
+        While the 2d arrays are constructed x first, then y
+        the debug functions read y then x so that you can group rows together, allowing line by line console entries.
+
+    */
+
+    // Variables
+
     // Num Grid
-    public int gridLength;
+    public Vector2Int gridDimensions;
     public int randomizerRange;
-    private List<List<int>> numGrid = new List<List<int>>{};
+    private int[,] numGrid;
 
     // Vertex Grid
     public int sectionSideLength;
     public int streetWidth;
 
     // Sector Grid
-    public List<List<Sector>> sectorGrid = new List<List<Sector>>{};
+    public Sector[,] sectorGrid;
     public int sectionCount;
 
-    // Mesh Creator Link
-    public Mesh_Creator Mesh_Creator;
+    public GameObject meshPrefab;
+
+    public List<MeshContainer> meshList = new List<MeshContainer> { };
+    public MeshContainer target;
+    public Material baseMaterial;
 
     // Debugging
-    public Text numbers;
-    public Text vertecies;
-    public Text sections;
+    public GameObject marker;
 
-    public void Start()
-    {
-        NumberGrid();
-        SectorGrid();
-        VertexFill();
-        Section();
 
-        // Render the grid
-        Mesh_Creator.CreateMeshObject();
-    }
-    
-    public void NumberGrid() // Creates a populated grid with random numbers within the "randomizerRange"
-    {
-        for (int a = 0; a < gridLength; a++)
-        {
-            List<int> numGridRow = new List<int>{};
 
-            for (int b = 0; b < gridLength; b++)
-            { numGridRow.Add(Random.Range(0, (0 + randomizerRange))); }
-
-            numGrid.Add(numGridRow);
-        }
-
-        // Debugging
-        var text = "Number Grid:";
-        for (int a = 0; a < gridLength; a++)
-        {
-            text = text + "\n\n";
-            for (int b = 0; b < gridLength; b++)
-            {
-                text = text + numGrid[a][b] + ", ";
-
-            }
-        }
-
-        numbers.text = text;
-    }
-
+    // Classes
     public class Connection // This class holds data on which edges of a sector border another sector of the same ID.
     {
         public bool left { get; set; }
@@ -82,9 +57,9 @@ public class Grid_Generator : MonoBehaviour
     public class Sector // This class holds data on ID, Vertexes, and Connections for one section within the sectorGrid.
     {
         public int? id { get; set; }
-        public List<Vector2> vertexPosition { get; set; }
+        public Vector2[] vertexPosition { get; set; }
         public Connection connected { get; set; }
-        public Sector(int? id, List<Vector2> vertexPosition, Connection connected)
+        public Sector(int? id, Vector2[] vertexPosition, Connection connected)
         {
             this.id = id;
             this.vertexPosition = vertexPosition;
@@ -92,65 +67,111 @@ public class Grid_Generator : MonoBehaviour
         }
     }
 
-    public void SectorGrid() // Creates an empty "sectorGrid"
+    public class MeshContainer
     {
-        for (int a = 0; a < gridLength; a++)
+        public GameObject gameObject { get; set; }
+        public List<Vector3> vertecies { get; set; }
+        public List<int> triangles { get; set; }
+        public MeshContainer(GameObject gameObject, List<Vector3> vertecies, List<int> triangles)
         {
-            List<Sector> sectorGridRow = new List<Sector>{};
-
-            for (int b = 0; b < gridLength; b++)
-            {
-                sectorGridRow.Add(new Sector(null, new List<Vector2>{}, new Connection(false, false, false, false)));
-            }
-            sectorGrid.Add(sectorGridRow);
+            this.gameObject = gameObject;
+            this.vertecies = vertecies;
+            this.triangles = triangles;
         }
+    }
+
+    public class VertexToggle
+    {
+        public bool topLeft { get; set; }
+        public bool topRight { get; set; }
+        public bool bottomLeft { get; set; }
+        public bool bottomRight { get; set; }
+        public VertexToggle(bool topLeft, bool topRight, bool bottomLeft, bool bottomRight)
+        {
+            this.topLeft = topLeft;
+            this.topRight = topRight;
+            this.bottomLeft = bottomLeft;
+            this.bottomRight = bottomRight;
+        }
+    }
+
+
+
+    // Functions
+    public void Start()
+    {
+        NumberGrid();
+        SectionGrid();
+        CreateMeshObject();
+
+        // Debugging
+        string text = "Connections: \n\n";
+        for (int y = 0; y < gridDimensions.y; y++)
+        {
+            for (int x = 0; x < gridDimensions.x; x++)
+            {
+                text = text + "Sector: (" + x + ", " + y + ") Left: " + sectorGrid[x,y].connected.left + " Right: " + sectorGrid[x, y].connected.right + " Up: " + sectorGrid[x, y].connected.up + " Down: " + sectorGrid[x, y].connected.down + "\n\n";
+            }
+        }
+
+        Debug.Log(text);
     }
     
-    public void VertexFill() // Fills the "sectorGrid" with four points on a 2D plane in the corners of each sector.
+    public void NumberGrid() // Creates a populated grid with random numbers within the "randomizerRange"
     {
-        for (int a = 0; a < gridLength; a++)
+        numGrid = new int[gridDimensions.x, gridDimensions.y];
+        for (int x = 0; x < gridDimensions.x; x++)
         {
-            for (int b = 0; b < gridLength; b++)
-            {
-                var x = a * (sectionSideLength + streetWidth);
-                var y = b * (sectionSideLength + streetWidth);
-
-                /*  [0][1]
-                    [2][3]  */
-                sectorGrid[a][b].vertexPosition.Add(new Vector2(x + streetWidth / 2,                        y - streetWidth / 2                     ));
-                sectorGrid[a][b].vertexPosition.Add(new Vector2(x - streetWidth / 2 + sectionSideLength,    y - streetWidth / 2                     ));
-                sectorGrid[a][b].vertexPosition.Add(new Vector2(x + streetWidth / 2,                        y + streetWidth / 2 - sectionSideLength ));
-                sectorGrid[a][b].vertexPosition.Add(new Vector2(x - streetWidth / 2 + sectionSideLength,    y + streetWidth / 2 - sectionSideLength ));
-            }
+            for (int y = 0; y < gridDimensions.y; y++)
+            { numGrid[x, y] = (Random.Range(0, (0 + randomizerRange))); }
         }
 
-        //Debug.Log("Vertex Grid (a,b)[c]:" + sectorGrid[a][b].vertexPosition[c]);
+
         // Debugging
-        var text = "Vertex Grid:";
-        for (int a = 0; a < gridLength; a++)
+        string text = "Numbers: \n\n";
+        for (int y = 0; y < gridDimensions.y; y++)
         {
-            text = text + "\n\n";
-            for (int b = 0; b < gridLength; b++)
+            for (int x = 0; x < gridDimensions.x; x++)
             {
-                text = text + sectorGrid[a][b].vertexPosition[0] + sectorGrid[a][b].vertexPosition[1] + sectorGrid[a][b].vertexPosition[2] + sectorGrid[a][b].vertexPosition[3] + ",  ";
-
+                text = text + numGrid[x, y] + ", ";
             }
+            text = text + "\n\n";
         }
-        vertecies.text = text;
+        Debug.Log(text);
     }
 
-    public void Section() // Sections "sectorGrid" based on "numGrid". Groups are formed according to "GridCrawler"
+    public void SectionGrid() // Creates a "sectorGrid" with four points on a 2D plane in the corners of each sector.
     {
+        sectorGrid = new Sector[gridDimensions.x, gridDimensions.y];
         int id = 0;
-        for (int a = 0; a < gridLength; a++)
+
+        for (int x = 0; x < gridDimensions.x; x++)
         {
-            List<int?> sectorGridCollumn = new List<int?> { };
-            for (int b = 0; b < gridLength; b++)
+            for (int y = 0; y < gridDimensions.y; y++)
             {
-                if (sectorGrid[a][b].id == null)
+                sectorGrid[x, y] = new Sector(null, new Vector2[4], new Connection(false, false, false, false));
+
+                var a = x * (sectionSideLength + streetWidth);
+                var b = y * (sectionSideLength + streetWidth);
+
+                // [0][1]
+                // [2][3]
+                sectorGrid[x,y].vertexPosition[0] = (new Vector2(a + streetWidth / 2,                        b - streetWidth / 2                     ));
+                sectorGrid[x,y].vertexPosition[1] = (new Vector2(a - streetWidth / 2 + sectionSideLength,    b - streetWidth / 2                     ));
+                sectorGrid[x,y].vertexPosition[2] = (new Vector2(a + streetWidth / 2,                        b + streetWidth / 2 - sectionSideLength ));
+                sectorGrid[x,y].vertexPosition[3] = (new Vector2(a - streetWidth / 2 + sectionSideLength,    b + streetWidth / 2 - sectionSideLength ));
+
+            }
+        }
+
+        for (int x = 0; x < gridDimensions.x; x++)
+        {
+            for (int y = 0; y < gridDimensions.y; y++)
+            {
+                if (sectorGrid[x, y].id == null)
                 {
-                    sectorGrid[a][b].id = id;
-                    GridCrawler(a, b, id);
+                    sectorGrid[x, y].id = id;
+                    GridCrawler(x, y, id);
                     id++;
                 }
             }
@@ -158,79 +179,195 @@ public class Grid_Generator : MonoBehaviour
         sectionCount = id;
 
         // Debugging
-        var text = "Sections Grid:";
-        for (int a = 0; a < gridLength; a++)
+        string text = "Sectors: \n\n";
+        for (int y = 0; y < gridDimensions.y; y++)
         {
-            text = text + "\n\n";
-            for (int b = 0; b < gridLength; b++)
+            for (int x = 0; x < gridDimensions.x; x++)
             {
-                text = text + sectorGrid[a][b].id + ", ";
-
+                text = text + sectorGrid[x,y].id + ", ";
             }
+            text = text + "\n\n";
         }
-        sections.text = text;
+        Debug.Log(text);
     }
 
     public void GridCrawler(int x, int y, int id) // Assign any cardinally connected numbers the same ID as the first, marks the connection, then repeats the check.
     {
         // Left
-        if (x - 1 > 0                   && numGrid[x][y] == numGrid[x - 1][y]   && sectorGrid[x - 1][y].id == null)
+        if (x - 1 > 0                   && numGrid[x,y] == numGrid[x - 1,y]   && sectorGrid[x - 1,y].id == null)
         {
-            ConnectionMarker(x, y, id);
-            sectorGrid[x - 1][y].id = id;
+            sectorGrid[x - 1,y].id = id;
             GridCrawler(x - 1, y, id);
         }
 
         // Right
-        if (x + 1 < numGrid.Count       && numGrid[x][y] == numGrid[x + 1][y]   && sectorGrid[x + 1][y].id == null)
+        if (x + 1 < gridDimensions.x    && numGrid[x,y] == numGrid[x + 1,y]   && sectorGrid[x + 1,y].id == null)
         {
-
-            ConnectionMarker(x, y, id);
-            sectorGrid[x + 1][y].id = id;
+            sectorGrid[x + 1,y].id = id;
             GridCrawler(x + 1, y, id);
         }
 
         // Up
-        if (y + 1 < numGrid[0].Count    && numGrid[x][y] == numGrid[x][y + 1]   && sectorGrid[x][y + 1].id == null)
+        if (y + 1 < gridDimensions.y    && numGrid[x,y] == numGrid[x,y + 1]   && sectorGrid[x,y + 1].id == null)
         {
-            ConnectionMarker(x, y, id);
-            sectorGrid[x][y + 1].id = id;
+            sectorGrid[x,y + 1].id = id;
             GridCrawler(x, y + 1, id);
         }
 
         // Down
-        if (y - 1 > 0                   && numGrid[x][y] == numGrid[x][y - 1]   && sectorGrid[x][y - 1].id == null)
+        if (y - 1 > 0                   && numGrid[x,y] == numGrid[x,y - 1]   && sectorGrid[x,y - 1].id == null)
         {
-            ConnectionMarker(x, y, id);
-            sectorGrid[x][y - 1].id = id;
+            sectorGrid[x,y - 1].id = id;
             GridCrawler(x, y - 1, id);
         }
+
+        ConnectionMarker(x, y, id);
     }
 
     public void ConnectionMarker(int x, int y, int id)
     {
         // Left
-        if (x - 1 > 0                   && numGrid[x][y] == numGrid[x - 1][y]   && sectorGrid[x - 1][y].connected.right == false)
+        if (x - 1 > 0                   && numGrid[x,y] == numGrid[x - 1,y]   && sectorGrid[x - 1,y].connected.right == false)
         {
-            sectorGrid[x][y].connected.left = true;
+            sectorGrid[x,y].connected.left = true;
         }
 
         // Right
-        if (x + 1 < numGrid.Count       && numGrid[x][y] == numGrid[x + 1][y]   && sectorGrid[x + 1][y].connected.left == false )
+        if (x + 1 < gridDimensions.x    && numGrid[x,y] == numGrid[x + 1,y]   && sectorGrid[x + 1,y].connected.left == false )
         {
-            sectorGrid[x][y].connected.right = true;
+            sectorGrid[x,y].connected.right = true;
         }
 
         // Up
-        if (y + 1 < numGrid[0].Count    && numGrid[x][y] == numGrid[x][y + 1]   && sectorGrid[x][y + 1].connected.down == false )
+        if (y + 1 < gridDimensions.y    && numGrid[x,y] == numGrid[x,y + 1]   && sectorGrid[x,y + 1].connected.down == false )
         {
-            sectorGrid[x][y].connected.up = true;
+            sectorGrid[x,y].connected.up = true;
         }
 
         // Down
-        if (y - 1 > 0                   && numGrid[x][y] == numGrid[x][y - 1]   && sectorGrid[x][y - 1].connected.up == false   )
+        if (y - 1 > 0                   && numGrid[x,y] == numGrid[x,y - 1]   && sectorGrid[x,y - 1].connected.up == false   )
         {
-            sectorGrid[x][y].connected.down = true;
+            sectorGrid[x,y].connected.down = true;
         }
+    }
+
+    public void CreateMeshObject()
+    {
+        for (int a = 0; a < sectionCount; a++)
+        {
+            // Create the mesh and target it
+            meshList.Add(new MeshContainer(Instantiate(meshPrefab, this.transform), new List<Vector3> { }, new List<int> { }));
+            target = meshList[meshList.Count - 1];
+
+            // Set Name
+            target.gameObject.name = ("Mesh (" + a + ")");
+
+            AssignValues(a);
+
+            for (int b = 0; b < target.vertecies.Count; b++)
+            {
+                GameObject newMarker = Instantiate(marker, new Vector3(target.vertecies[b].x, 5, target.vertecies[b].z), new Quaternion());
+                newMarker.name = ("Mesh: " + a + " Marker: " + b);
+            }
+        }
+    }
+    
+    public void AssignValues(int id)
+    {
+        VertexToggle enabledVertexes = new VertexToggle(true, true, true, true);
+
+        // for each section, check connections and ID. if connected and the ID matches, add vertecies to the list. If not connected, add a different set to the list
+        for (int x = 0; x < gridDimensions.x; x++)
+        {
+            for (int y = 0; y < gridDimensions.x; y++)
+            {
+
+                if (sectorGrid[x, y].id == id)
+                {
+
+                    if (sectorGrid[x, y].connected.left == true)
+                    {
+                        enabledVertexes.topLeft = false;
+                        enabledVertexes.bottomLeft = false;
+                    }
+
+                    if (sectorGrid[x, y].connected.right == true)
+                    {
+                        enabledVertexes.topRight = false;
+                        enabledVertexes.bottomRight = false;
+                    }
+
+                    if (sectorGrid[x, y].connected.up == true)
+                    {
+                        enabledVertexes.topLeft = false;
+                        enabledVertexes.topRight = false;
+                    }
+
+                    if (sectorGrid[x, y].connected.down == true)
+                    {
+                        enabledVertexes.bottomLeft = false;
+                        enabledVertexes.bottomRight = false;
+                    }
+
+                    if (enabledVertexes.topLeft)
+                    {
+                        target.vertecies.Add(new Vector3(sectorGrid[x, y].vertexPosition[0].y, 0, sectorGrid[x, y].vertexPosition[0].x));
+                    }
+                    if (enabledVertexes.topRight)
+                    {
+                        target.vertecies.Add(new Vector3(sectorGrid[x, y].vertexPosition[1].y, 0, sectorGrid[x, y].vertexPosition[1].x));
+                    }
+                    if (enabledVertexes.bottomLeft)
+                    {
+                        target.vertecies.Add(new Vector3(sectorGrid[x, y].vertexPosition[2].y, 0, sectorGrid[x, y].vertexPosition[2].x));
+                    }
+                    if (enabledVertexes.bottomRight)
+                    {
+                        target.vertecies.Add(new Vector3(sectorGrid[x, y].vertexPosition[3].y, 0, sectorGrid[x, y].vertexPosition[3].x));
+                    }
+
+                    //for (int c = 0; c < sectorGrid[x, y].vertexPosition.Count; c++)
+                    //{
+                    //    target.vertecies.Add(new Vector3(sectorGrid[x, y].vertexPosition[c].y, 0, -sectorGrid[x, y].vertexPosition[c].x));
+                    //}
+                }
+            }
+        }
+
+        //Create triangles out of vertecies
+        for (int a = 0; a < target.vertecies.Count; a += 4)
+        {
+            // [0][1]
+            // [2][3]
+
+            target.triangles.Add(a);
+            target.triangles.Add(a + 1);
+            target.triangles.Add(a + 2);
+
+            target.triangles.Add(a + 1);
+            target.triangles.Add(a + 3);
+            target.triangles.Add(a + 2);
+        }
+
+
+        string text = "ID: " + id + " | # of Vertecies: " + target.vertecies.Count + " | " + "Coordinates: ";
+        for (int a = 0; a < target.vertecies.Count; a++)
+        {
+            text = text + target.vertecies[a] + ", ";
+        }
+        Debug.Log(text);
+
+        SendMesh();
+    }
+
+    public void SendMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = target.vertecies.ToArray();
+        mesh.triangles = target.triangles.ToArray();
+
+        target.gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        target.gameObject.GetComponent<MeshRenderer>().material = baseMaterial;
+        target.gameObject.GetComponent<MeshRenderer>().material.color = Random.ColorHSV(0f, 1f, .5f, .5f, .25f, .75f, 1f, 1f);
     }
 }
