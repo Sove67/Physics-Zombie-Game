555cbad22bc8fc06ac5a811437bcdba2884a67c5
Kaleb Afseth <afseth598@gmail.com>
2020-02-25 18:34:22 -0600

    Character Attachment
    The player model is now attached, and partially rigged. Need more animations and the turn to camera animation implemented.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
     private CharacterController charControl;
-    public Transform playerDirection;
+    public Transform player;
+    public Transform model;
+    public Transform playerXRot;
+    public Animator animator;
+    public float localRot;
+    public float TorsoRot;
+    public float modelRot;
+    public int lookMargin;
     public float moveMulti = 40f;
     public float jumpMulti = 20f;
     public float GravMulti = 20f;
@@ -18,16 +26,51 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
+        Movement();
+        Animate();
+    }
+    public void Movement()
+    {
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), Convert.ToInt32(Input.GetButton("Jump")), Input.GetAxis("Vertical"));
+        Vector3 localInput = playerXRot.TransformDirection(moveInput.x, 0, moveInput.z);
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        { localRot = ((Mathf.Atan2(localInput.x, localInput.z) / Mathf.PI * 180) + 360) % 360; }
+
+        TorsoRot = ((localRot - playerXRot.rotation.eulerAngles.y) + 360 + 90) % 360;
+
         if (charControl.isGrounded)
         {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = playerDirection.TransformDirection(moveDirection);
-            moveDirection *= moveMulti;
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpMulti;
+            moveDirection = localInput;
+            moveDirection.x *= moveMulti;
+            moveDirection.z *= moveMulti;
+            moveDirection.y *= jumpMulti;
         }
+
         charControl.Move(moveDirection * Time.deltaTime);
         moveDirection.y -= GravMulti * Time.deltaTime;
-        
+    }
+
+    public void Animate()
+    {
+        float oldModelRot = modelRot;
+
+        if (((TorsoRot + 1) % 360) - 1 >= (-lookMargin) % 360 && ((TorsoRot + 1) % 360) - 1 < (180 + lookMargin) % 360)
+        {
+            animator.SetBool("Backpedal", false);
+            modelRot = localRot;
+            model.transform.rotation = Quaternion.Euler(0, modelRot, 0);
+        }
+        else
+        {
+            animator.SetBool("Backpedal", true);
+            modelRot = localRot - 180;
+            model.transform.rotation = Quaternion.Euler(0, modelRot - 180, 0);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        { animator.SetBool("Moving", true); }
+        else { animator.SetBool("Moving", false); }
+        // animate torso turn to modelRot from old modelRot
     }
 }
